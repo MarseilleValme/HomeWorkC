89906b81416352ce9a899422d57ddf8c4ab2576e
Grigoriev_Sergey <gsn1232@gmail.com>
2023-03-07 19:05:11 +0200

    init

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+// Интерполяция строк
+// Console.WriteLine($"Результат {a} + {b} = {a + b}");
+// Console.WriteLine("Результат " + a + " + " + b + " = " + (a + b));
+// Console.WriteLine(a > b);
+if (a > b)
+    Console.WriteLine(a);
+else if (a < b)
+    Console.WriteLine(b);
+else
+    Console.WriteLine("=");
