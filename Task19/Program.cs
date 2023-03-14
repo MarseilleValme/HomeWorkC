2618041774ddb06ca70bcebad55855efc0fa8af1
Grigoriev_Sergey <gsn1232@gmail.com>
2023-03-14 18:36:05 +0200

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число");
+string s = Console.ReadLine()!;
+int l = s.Length;
+int sum = 0;
+for (int i = 0; i < l; i++)
+    sum = int.Parse(s[i].ToString()) - int.Parse(s[l - i - 1].ToString());
+if (sum == 0)
+    Console.WriteLine("Число является палиндромом");    
+else 
+    Console.WriteLine("Число палиндромом не является");   
