809a25d978f373a9ea0ae4addbb3aaa5d94cfb3b
Grigoriev_Sergey <gsn1232@gmail.com>
2023-03-14 13:36:15 +0200

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите координату A(x): ");
+double ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату A(y): ");
+double ay = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату A(z): ");
+double az = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату B(x): ");
+double bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату B(y): ");
+double by = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату B(z): ");
+double bz = Convert.ToDouble(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+Console.WriteLine($"Расстояние между двумя точками равно {Math.Round(s, 2)}");
