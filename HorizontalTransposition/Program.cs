36f241e8583e739ef37b05f11138a010427887f7
Grigoriev_Sergey <gsn1232@gmail.com>
2023-03-31 03:53:31 +0300

    HorizontalTransposition

---
+++ HorizontalTransposition/Program.cs
@@ -0,0 +1,41 @@
+﻿//Horizontal Transposition (реверс двумерной матрицы относительно горизонтали)
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 101); // [0, 100]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void HorizontalTransposition(int[,] matrix)
+{
+    int box, n = matrix.GetLength(0);       // Удобства восприятия 29-й и 30-й строчек для
+    for (int i = 0; i <= n / 2 - 1; i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            box = matrix[i, j];
+            matrix[i, j] = matrix[n - 1 - i, j];
+            matrix[n - 1 - i, j] = box;
+        }
+}
+Console.Clear();
+int m = 4, n = 5;
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.WriteLine("Исходная матрица:");
+PrintMatrix(matrix);
+HorizontalTransposition(matrix);
+Console.WriteLine("Транспонированная матрица:");
+PrintMatrix(matrix);
